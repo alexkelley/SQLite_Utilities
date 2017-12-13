61bad80fec4417467f31d4cc6f6b6174881c688a
Alex Kelley <alexkelley@yahoo.com>
2017-12-13 07:16:43 -0600

    user interface for column labels

--- load_csv.py
+++ load_csv.py
@@ -8,7 +8,7 @@ def prompt_for_filename():
     '''
     Returns a string of the filename
     '''
-    filename = input("Data file to load (enclose in quotes) >> ")
+    filename = input("Data file to load >> ")
 
     return filename
 
@@ -21,21 +21,42 @@ def get_file_size(filename):
     '''
     if os.path.isfile(filename):
         file_info = os.stat(filename)
+        return file_info.st_size
 
-    return file_info.st_size
+    else:
+        return 0
 
 
-def user_prompt_column_labels():
+def user_prompt_column_labels(first_row):
     '''
     Find out from user if first row contains column names
     Provide an option to edit or create column labels
 
     Returns a list of column names
     '''
-    pass
+
+    print(first_row)
+    flag = input('Does the first row contain column labels (Y/N)? >> ')
+
+    if flag == 'Y':
+        flag = True
+    else:
+        flag = False
+        
+    column_labels = []
+    for i, label in enumerate(first_row):
+        print('{0}: {1}'.format(i, label))
+        new_label = input('New label for {} (blank leaves label unchanged) >> '.format(i))
+
+        if new_label:
+            column_labels.append(new_label)
+        else:
+            column_labels.append(label)
+
+    return (column_labels, flag)
 
     
-def read_csv(filename, column_names):
+def read_csv(filename):
     '''
     Parameters:
         - string filename
@@ -46,12 +67,19 @@ def read_csv(filename, column_names):
     data_list = []
     temp_dict = {}
     
-    with open(filename, 'rb') as f:
+    with open(filename, 'r') as f:
         reader = csv.reader(f)
-        # parse & display to user the first row to determine if they are column labels
-        # user_prompt_column_labels()
+
+        first_row = next(reader)
+
+        column_names, flag = user_prompt_column_labels(first_row)
+        print(column_names)
+
+        if not flag:
+            reader.seek(0)
+
         for row in reader:
-            pass
+            data_list.append(row)
 
     return data_list
 
@@ -66,8 +94,13 @@ def main():
     # check if filesize is acceptable
     if get_file_size(filename) < 1000000000:
         print('\nfile:{} is not too big.\n'.format(filename))
+
+    data = read_csv(filename)
+
+    #print(data[:2])
+    
     # build list of column labels
-    # read in csv and return list of dictionaries {'col_name1': 'field_value', 'col_name2': 'field_value',}
+    # return list of dictionaries {'col_name1': 'field_value', 'col_name2': 'field_value',}
 
 
 if __name__ == '__main__':
