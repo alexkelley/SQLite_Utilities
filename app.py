02a06255215410e5d32a32c4171d69cbf759c7fe
alexkelley <alexkelley@yahoo.com>
2017-12-26 17:32:57 -0600

    not receiving the POST data

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from pprint import pprint as pp
+import os
 
 from flask import Flask, render_template, request
+
+from flask_wtf import FlaskForm
+#from flask_wtf.file import FileField, FileRequired
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 from attribute_table import build_attributes, build_key_string
 from load_csv import load_csv_main
@@ -9,21 +14,29 @@ from database_calls import create_database, load_data_into_table
 
 app = Flask(__name__)
 
-
 with open('app.secrets', 'r') as f:
     secret_key = f.read().strip()
 
 app.config['SECRET_KEY'] = secret_key
 
 
-@app.route('/')
-@app.route('/', methods=['POST'])
+class UploadForm(FlaskForm):
+    #csv_file = FileField('Select a file to upload >> ', validators=[FileRequired()])
+    csv_file = StringField('Enter some data:', validators=[DataRequired()])
+    
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    data = ''
-    if request.method == 'POST':
-        filename = form
-        data = ['success', filename]
-    return render_template("base.html", data=data)
+    csv_file = None
+    data = None
+    form = UploadForm()
+    if form.validate_on_submit():
+        print('in')
+        f = form.csv_file.data
+        data = f.filename
+        print(data)
+
+    print('out')
+    return render_template('base.html', form=form, data=data)
 
 
 if __name__ == "__main__":
