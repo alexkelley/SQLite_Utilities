224a65b22a9bd5a6c2f756efcad32aa462b53e62
alexkelley <alexkelley@yahoo.com>
2017-12-26 08:51:51 -0600

    forming up web interface

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from pprint import pprint as pp
 
 from flask import Flask, render_template, request
 
-# import main logic from other files
-#from weather import get_local_time, query_api
+from attribute_table import build_attributes, build_key_string
+from load_csv import load_csv_main
+from database_calls import create_database, load_data_into_table
+
 
 app = Flask(__name__)
 
@@ -11,7 +13,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/', methods=['POST'])
 def index():
-    data = []
+
+    # get filename from webform
+    
+    data, column_names = load_csv_main(filename)
+
+    # display csv data in tabular format
+    # ui for specifying db_name, table_name and column_names
     error = None
     if request.method == 'POST':
         data.append('success')
