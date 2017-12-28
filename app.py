7e809da7b84095bfec7db4b1c7b69411ab4da66d
alexkelley <alexkelley@yahoo.com>
2017-12-28 10:30:21 -0600

    save csv files to local storage

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, request, flash, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
 
 from forms import UploadForm, ColumnLabelForm
 
@@ -24,23 +25,29 @@ bootstrap = Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    csv_file = None
-    data = None
     form = UploadForm()
+    
     if request.method == 'POST':
         if form.validate() == False:
             flash('All fields are required.')
         else:
             raw_data = form.csv_file.data
-            session['csv_file'] = raw_data.read()
+            filename = secure_filename(raw_data.filename)
+            raw_data.save(os.path.join(
+                app.instance_path, 'csv_files', filename
+            ))
+            
+            session['csv_filename'] = filename
+            
             return redirect(url_for('column_names'))
             
-    return render_template('index.html', form=form, data=data)
+    return render_template('index.html', form=form)
+
 
 @app.route('/column_names', methods=['GET', 'POST'])
 def column_names():
     form = ColumnLabelForm()
-    data = session['csv_file']
+    data = session['csv_filename']
     return render_template('column_names.html', form=form, data=data)
 
 
--- forms.py
+++ forms.py
@@ -4,10 +4,12 @@ from wtforms.validators import Required
 from flask_wtf.file import FileField, FileRequired
 from werkzeug.utils import secure_filename
 
+
 class UploadForm(FlaskForm):
     csv_file = FileField('Select a file to upload >> ', validators=[FileRequired()])
     submit = SubmitField()
 
+    
 class ColumnLabelForm(FlaskForm):
     db_name = StringField('Enter a database name:', validators=[Required()])
     table_name = StringField('Enter a table name:', validators=[Required()])
