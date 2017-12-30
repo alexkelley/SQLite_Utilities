d309272b5370803dd62704bfb28938efe6f641d7
alexkelley <alexkelley@yahoo.com>
2017-12-30 08:59:12 -0600

    consolidation of shared utilities between web and CLI apps

--- shared_utilities.py
+++ shared_utilities.py
@@ -5,7 +5,43 @@ import os
 import itertools
 import datetime
 import re
+import sqlite3
+import sys
 from pprint import pprint as pp
+
+
+def generate_insert(table_name, field_value_dict):
+    '''
+    Parameters:
+        1. string table_name
+        2. dictionary of values in the form:
+           {column_name1: field_value, 
+            column_name2: field_value,
+            ...}
+    
+    Returns:
+        - string SQLite3 INSERT statement
+    '''
+    value_string = ''
+    column_string = ''
+
+    for label, value in field_value_dict.items():
+        if value is None:
+            value = 'NULL'
+
+        if value != 'NULL' and isinstance(value, str):
+            value_string += "'{}', ".format(value.replace("'", "").replace("%", "").replace("$", "").replace(",", ""))
+        elif isinstance(value, datetime.date):
+            value_string += "'{}', ".format(value)
+        else:
+            value_string += "{}, ".format(value)
+
+        column_string += "{}, ".format(label.replace("'", ""))
+
+        sql_string = "INSERT INTO {0} ({1}) VALUES ({2});".format(
+            table_name, column_string[:-2], value_string[:-2])
+
+    return sql_string
 
     
 def read_csv(filename):
@@ -56,7 +92,6 @@ def clean_column_name(field_value):
 
 
     return data
-
 
 
 def clean_data_value(field_value):
@@ -120,6 +155,89 @@ def build_column_data(data_dict):
 
     return (column_list, attributes)
 
+
+def load_data_into_table(db_name, table_name, data_list):
+    '''
+    
+    '''
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()    
+
+    for record in data_list:
+        sql_call = generate_insert(table_name, record)
+
+        try:
+            cursor.execute(sql_call)
+        except:
+            print(sys.exc_info())
+            print(sql_call)
+            break
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def create_database(db_name, table_name, attribute_dict, primary_key):
+    '''
+    Parameters:
+        1. string database name
+        2. string table name
+        3. dictionary of table attributes
+            'table': {
+                0: {'column_name': 'ID', 'data_type': 'INTEGER'},
+                1: {'column_name': 'backend', 'data_type': 'TEXT'},
+                ...
+            }
+
+        4. string for primary key
+           primary_key = 
+              'CONSTRAINT transaction_pk
+                   PRIMARY KEY (transaction_id, company_id),
+
+               CONSTRAINT transactions_fk
+                   FOREIGN KEY (company_id)
+                       REFERENCES company(company_id)'
+
+    Returns:
+        - True if successful
+        - False if not successful
+    '''
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    cursor.execute('DROP TABLE IF EXISTS {}'.format(table_name))
+    
+    build_table_sql = 'CREATE TABLE {} (\n'.format(table_name)
+
+
+    for key, value in attribute_dict.items():
+        build_table_sql += "\t{} {},\n".format(value['name'], value['data_type'])
+
+    build_table_sql += '\n\t{}\n);'.format(primary_key)
+    
+    try:
+        cursor.execute(build_table_sql)
+        flag = True
+        print('{} database created.'.format(db_name))
+
+    except:
+        print(sys.exc_info())
+        print(build_table_sql)
+        flag = False
+
+    finally:
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    return flag
+
+
+
+#################
+# Testing Calls #
+#################
 
 if __name__ == "__main__":
 
